de8e4a0f6dd5134cdbd6e49fa24855289b9c7939
Joseph Maldjian <josephavm@icloud.com>
2023-09-08 01:34:56 -0500

    update home url in footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,7 +24,7 @@ export default function App(props: AppProps) {
       >
             <Navigation links={[{label: "Home", link: "/"}, {label: "Join", link: "/join"}, {label: "Our Team", link: "/team"}]} />
         <Component {...pageProps} />
-        <Footer links={[{label: "Home", link: "/home"}, {label: "Join", link: "/join"}, {label: "Our Team", link: "/team"}, {label: "Source Code", link: "https://github.com/hye-dev/website"}]} />
+        <Footer links={[{label: "Home", link: "/"}, {label: "Join", link: "/join"}, {label: "Our Team", link: "/team"}, {label: "Source Code", link: "https://github.com/hye-dev/website"}]} />
       </MantineProvider>
     </>
   );
